56716c9e4eca02666b038d8e907f369a71bf5486
Rubaiyat Jahan Mumu <rubaiyat.jahan@gmail.com>
2020-02-17 15:08:56 -0500

    Searchbar added

--- VocaPower/VocaPower/Home/MainViewController.swift
+++ VocaPower/VocaPower/Home/MainViewController.swift
@@ -17,17 +17,41 @@ class MainViewController: UIViewController {
 
     @IBOutlet weak var tableview: UITableView!
     let data: [CellDataModel] = [ CellDataModel(header: "Barrons 333", text: "Study These Words"), CellDataModel(header: "Barrons 800", text: "Study These Words"), CellDataModel(header: "Maghoosh 1000", text: "Study These Words"), CellDataModel(header: "Word Smart I", text: "Study These Words"), CellDataModel(header: "Manhattan Essential", text: "Study These Words"), CellDataModel(header: "Favourite", text: "Study These Words"), CellDataModel(header: "Help & Feedback", text: "Rate Us")]
+
+    lazy var searchBar:UISearchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 40, height: 20))
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Main"
+        //title = "Main"
         setupTableView()
+        setupSearchbar()
+        navigationController?.navigationBar.barTintColor = UIColor(red: 88.0/255.0, green: 86.0/255.0, blue: 214.0/255.0, alpha: 1)
+        self.view.backgroundColor = UIColor(red: 88.0/255.0, green: 86.0/255.0, blue: 214.0/255.0, alpha: 1)
+    }
+
+    func setupSearchbar() {
+
+
+        searchBar.searchBarStyle = .default
+        //view.addSubview(searchBar)
+
+        searchBar.placeholder = "Search words...    "
+        searchBar.set(textColor: .black)
+        searchBar.setTextField(color: UIColor.white)
+        searchBar.setPlaceholder(textColor: .black)
+        searchBar.setSearchImage(color: .black)
+        searchBar.setClearButton(color: .red)
+        let leftNavBarButton = UIBarButtonItem(customView:searchBar)
+        self.navigationItem.leftBarButtonItem = leftNavBarButton
     }
 
     func setupTableView() {
         tableview.delegate = self
         tableview.dataSource = self
         tableview.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "MainCell")
+        tableview.rowHeight = UITableView.automaticDimension
+        tableview.estimatedRowHeight = 200
+        tableview.backgroundColor = UIColor(red: 88.0/255.0, green: 86.0/255.0, blue: 214.0/255.0, alpha: 1)
     }
 }
 
